3022eaa8583d4625588b1d85d3ca926f39deed9a
Lina Balciunaite <lina_balciunaite@yahoo.com>
2023-02-16 10:31:03 +0000

    new branch for Messages of the game - explore the possibility to have colourful messages

--- src/server/messages/GameMessages.java
+++ src/server/messages/GameMessages.java
@@ -1,5 +1,13 @@
 package server.messages;
 
-public abstract class GameMessages {
+public final class GameMessages {
+
     public static final String WELCOME = "Welcome to quiz!";
+
+    public static final String WRONG_ANSWER = "\u001B[31m" + "WRONG." + "\u001B[0m" + "The right answer is: X";
+
+    // For testing purposes, delete it after
+    public static void main(String[] args) {
+        System.out.println(GameMessages.WRONG_ANSWER);
+    }
 }
